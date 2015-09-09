d7ce8c1f3991675bf391592815721c15a5fbff95
kordianbruck <kb@wdkk.de>
2015-09-09 11:00:52 +0200

    Put configuration in separate file

--- AutoUpdate.php
+++ AutoUpdate.php
@@ -1,11 +1,8 @@
 <?php
-
-//Edit these values to fit your needs
-define('PATH_REPOSITORY', '/var/www/git-repo/'); //With trailing slash
-define('PATH_PUBLIC', '/var/www/public_html/'); //With trailing slash
-define('GIT_BRANCH', 'master'); //Use this branch as your development branch and only update this branch
-define('GITHUB_SECRET', ''); //Set your secret here in order to verify that requests come from Github
-//Do not edit below
+if(!file_exists('Config.php')){
+    exit('Please configure this app first by copying the default config to Config.php');
+}
+include 'Config.php';
 
 //Handle incoming github webhooks
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
